bdd73757981e057f3ed95be1b64d9006d6d54e3e
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2018-11-07 13:27:39 -0800

    Se implemento la funcion de editar un post con su respectivo test

--- Udemy/ruby-rails-5-bdd-rspec-capybara/blog_app/spec/requests/articles_spec.rb
+++ Udemy/ruby-rails-5-bdd-rspec-capybara/blog_app/spec/requests/articles_spec.rb
@@ -16,15 +16,15 @@ RSpec.describe "Articles", type: :request do
     end
 
     context 'with non-existing article' do
-      before {get "/articles/xxxx"}
+      before {get "/articles/99999"}
 
       it "handless non-existing article" do
-        expect(response.status).to eq 404
+        expect(response.status).to eq 302
         flash_message = "The article you are looking for could not be found"
         expect(flash[:alert]).to eq flash_message
       end
     end
 
-  end
 
+  end
 end
