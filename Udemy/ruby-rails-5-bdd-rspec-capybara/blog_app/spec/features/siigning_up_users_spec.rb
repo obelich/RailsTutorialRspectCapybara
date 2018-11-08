eaea59e6964e5a30a4dcb7168abd47dcce5cb6b8
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2018-11-08 12:36:02 -0800

    Implementando registro de usuario y desautentificacion con su test

---
+++ Udemy/ruby-rails-5-bdd-rspec-capybara/blog_app/spec/features/siigning_up_users_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.feature "Signup users" do
+
+  scenario "with valid credentias" do
+    visit "/"
+
+    click_link "Sign up"
+    fill_in "Email", with: 'user@example.com'
+    fill_in "Password", with: 'password'
+    fill_in "Password confirmation", with: 'password'
+
+    click_button "Sign up"
+    expect(page).to have_content("You have signed up successfully")
+  end
+
+  scenario "with invalid credentials" do
+    visit "/"
+
+    click_link "Sign up"
+    fill_in "Email", with: ''
+    fill_in "Password", with: ''
+    fill_in "Password confirmation", with: ''
+
+    click_button "Sign up"
+    expect(page).to have_content("2 errors prohibited this user from being saved")
+  end
+
+end
