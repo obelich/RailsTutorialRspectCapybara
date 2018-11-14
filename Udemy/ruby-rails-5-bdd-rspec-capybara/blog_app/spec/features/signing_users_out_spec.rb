4c745f7200db20e1db3d1223b279985dd550162e
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2018-11-14 11:56:33 -0800

    Agregado test para cerrar sesion

--- Udemy/ruby-rails-5-bdd-rspec-capybara/blog_app/spec/features/signing_users_out_spec.rb
+++ Udemy/ruby-rails-5-bdd-rspec-capybara/blog_app/spec/features/signing_users_out_spec.rb
@@ -1,9 +1,22 @@
 require "rails_helper"
 
-RSpec.feature "Users Signin" do
+RSpec.feature "Signing out signed-in users" do
 
   before do
     @user = User.create!(email: 'user@example.com', password: 'password')
+
+    visit '/'
+    click_link "Login"
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: @user.password
+    click_button "Log in"
+  end
+
+  scenario do
+    visit "/"
+    click_link "Sign out"
+    expect(page).to have_content("Signed out successfully")
+    expect(page).not_to have_content("Sign out")
   end
 
 end
