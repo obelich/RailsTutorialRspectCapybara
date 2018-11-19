3d420d5f20480f472c20eaf3db78037c380d12fe
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2018-11-19 13:08:23 -0800

    preparando testing para la creacion de comentarios para los articulos

---
+++ Udemy/ruby-rails-5-bdd-rspec-capybara/blog_app/spec/features/adding_comments_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.feature 'Adding reviews to Articles' do
+  before do
+    @user = User.create!(email: 'user@example.com', password: 'password')
+    @user1 = User.create!(email: 'user1@example.com', password: 'password1')
+    @user2 = User.create!(email: 'user2@example.com', password: 'password1')
+    @user3 = User.create!(email: 'user3@example.com', password: 'password1')
+
+    @article = Article.create!(title: "The first article", body: "Lorem ipsum one", user: @user)
+
+  end
+
+  scenario "permits a signed in user to write a review" do
+    login_as(@user1)
+
+    visit "/"
+
+    click_link @article.title
+    fill_in "New Comment", with: "An amazing article"
+    click_button "Add Comment"
+
+    expect(page).to have_content("Comment has been created")
+    expect(page).to have_content("An amazing article")
+    expect(current_path).to eq(article_path(@article.id))
+
+  end
+
+end
