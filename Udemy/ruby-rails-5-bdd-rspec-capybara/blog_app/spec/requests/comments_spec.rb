c3623984fb32a9fa96c309b20b87de950a9b41be
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2018-11-26 13:19:38 -0800

    generada la prueba para comentarios

--- Udemy/ruby-rails-5-bdd-rspec-capybara/blog_app/spec/requests/comments_spec.rb
+++ Udemy/ruby-rails-5-bdd-rspec-capybara/blog_app/spec/requests/comments_spec.rb
@@ -25,8 +25,17 @@ RSpec.describe "Comments", type: :request do
 
     context 'with a logged in user' do
       before do
-        post "/articles/#{@article.id}/comments", params: {comment: {body: "Awesome blog"}}
         login_as(@user1)
+        post "/articles/#{@article.id}/comments", params: {comment: {body: "Awesome blog"}}
+
+      end
+
+      it "Create the comment successfully" do
+        flash_message = "Comment has been created"
+        expect(response).to redirect_to(article_path(@article.id))
+        expect(response.status).to eq 302
+        expect(flash[:notice]).to eq flash_message
+
       end
 
     end
